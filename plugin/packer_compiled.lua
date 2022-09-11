15407a084c66ec8ecca7de039f4d0a27f245ff47
Amir Seraj <amirseraj.ir@gmail.com>
2022-09-11 13:33:01 +0430

    ToggleTerm added

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -219,6 +219,11 @@ _G.packer_plugins = {
     path = "/home/mummy/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
+  ["toggleterm.nvim"] = {
+    loaded = true,
+    path = "/home/mummy/.local/share/nvim/site/pack/packer/start/toggleterm.nvim",
+    url = "https://github.com/akinsho/toggleterm.nvim"
+  },
   ["vim-bbye"] = {
     loaded = true,
     path = "/home/mummy/.local/share/nvim/site/pack/packer/start/vim-bbye",
